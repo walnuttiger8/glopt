8ce801d528f2595bfe3688ea83a1ae6195368041
Bulat <bulatsharipov240@gmail.com>
2019-10-27 21:31:03 +0300

    computer version done

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.feed__inner').slick({
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+    });
+  });
